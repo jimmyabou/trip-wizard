d0fc58096289d44b721449a4a92c27716905e8d6
jamilaboudib <jimmy_aboudib@hotmail.com>
2023-07-08 12:37:19 -0400

    react dom router library and added route to register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,24 +2,44 @@ import logo from './logo.svg';
 import './App.css';
 import Fetch from './hooks/fetchUsers';
 import UserForm from './components/userForm';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 
 function App() {
   const{ data}=Fetch();
   return (
-    <div className="App">
-    <UserForm/>
-    {data && (
-     <ul>
-      {data.users.map(user => (
-     <li key={user.id}>
-      Email: {user.email}, Password: {user.password}
-   </li>
-    ))}
-    </ul>
-    )}
-  </div>
+    <>
+
+    <Router>
+      <div>
+        <h1>Main App Page</h1>
+        <p>This is the main page of your app.</p>
+        
+        <Link to="/userform">register</Link>
+
+        <Routes>
+        <Route path="/userform" element={<UserForm />} />
+        </Routes>
+      </div>
+    </Router></>
   );
-}
+};
+
 
 export default App;
+
+// return (
+//   <div className="App">
+//   <UserForm/>
+// </div>
+// );
+//_________________________________
+// {data && (
+  //  <ul>
+//   {data.users.map(user => (
+//  <li key={user.id}>
+//   Email: {user.email}, Password: {user.password}
+// </li>
+// ))}
+// </ul>
+// )}
