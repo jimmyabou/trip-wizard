7dd7da4f3fa3676e9c8d2c98390f95e4ee3318b1
jamilaboudib <jimmy_aboudib@hotmail.com>
2023-07-08 14:43:34 -0400

    modify route on app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,6 @@ import Fetch from './hooks/fetchUsers';
 import UserForm from './components/userForm';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Navbar from './components/NavBar';
-import userForm from './components/userForm';
 
 
 
