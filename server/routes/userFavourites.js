bb2c9e50681f5d0e3ac1271d7d74860267966611
Tamara Tell <2telltamara@gmail.com>
2023-07-10 15:21:38 -0400

    add post route for favourites

--- server/routes/userFavourites.js
+++ server/routes/userFavourites.js
@@ -8,5 +8,17 @@ router.get('/favourites', (req, res) => {
   });
 });
 
+router.post('/favorites', async (req, res) => {
+  const { user_id, attraction_id } = req.body;
+
+  try {
+    const result = await handleUserFav(user_id, attraction_id);
+    res.status(200).json({ message: result });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to favorite attraction' });
+  }
+});
+
+
 
 module.exports = router;
