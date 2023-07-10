cd6a98107586eb9fdad6ee62691b7a6917debe8d
Tamara Tell <2telltamara@gmail.com>
2023-07-10 18:50:33 -0400

    create custom hook fetchFavAttractions

---
+++ client/src/hooks/attractions/fetchFavAttractions.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const FetchFavAttractions = ({ userId }) => {
+  const [favAttractionsData, setFavAttractionsData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchFavData = async () => {
+      try {
+        const response = await axios.get(`/favorites/${userId}`);
+        console.log(response.data);
+        setFavAttractionsData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchFavData();
+  }, [userId]);
+
+  return (
+    <div>
+      {/* You can render your data here */}
+      {isLoading && <div>Loading...</div>}
+      {error && <div>{error.message}</div>}
+      {!isLoading && favAttractionsData && (
+        <div>
+          {/* Render your attractions data here */}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FetchFavAttractions;
