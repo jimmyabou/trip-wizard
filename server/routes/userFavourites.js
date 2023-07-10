f5cf22820cbdd9b28f5919819f931b31876eae08
Tamara Tell <2telltamara@gmail.com>
2023-07-10 15:24:31 -0400

    change userFavourites routes

--- server/routes/userFavourites.js
+++ server/routes/userFavourites.js
@@ -3,22 +3,26 @@ const router = express.Router();
 const userFavourites = require('../db/queries/userFavourites');
 
 router.get('/favourites', (req, res) => {
-  attractions.getFeaturedAttractions().then(data => {
-    res.json({ attractions: data });
-  });
+  const { user_id } = req.query;
+
+  userFavourites.getUserFavAttractions(user_id)
+    .then(data => {
+      res.json({ attractions: data });
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Failed to fetch user favorites' });
+    });
 });
 
 router.post('/favorites', async (req, res) => {
   const { user_id, attraction_id } = req.body;
 
   try {
-    const result = await handleUserFav(user_id, attraction_id);
+    const result = await userFavourites.handleUserFav(user_id, attraction_id);
     res.status(200).json({ message: result });
   } catch (error) {
     res.status(500).json({ error: 'Failed to favorite attraction' });
   }
 });
-
-
 
 module.exports = router;
