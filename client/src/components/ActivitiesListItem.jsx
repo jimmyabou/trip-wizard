07a8053762ecfbd749c3858b4bfa9aa0780206f8
Tamara Tell <2telltamara@gmail.com>
2023-07-10 12:28:42 -0400

    fix styles

--- client/src/components/ActivitiesListItem.jsx
+++ client/src/components/ActivitiesListItem.jsx
@@ -1,29 +1,33 @@
 import React from 'react';
 
+import EnhancedActivitiesListItem from './EnhancedActivitiesListItem';
+
 const ActivitiesListItem = (props) => {
 
   const { attraction } = props;
 
   return (
-    < div className="activity-list__item" key={attraction.attraction_id} >
-      <button className="fav-button">
-        <i className="fa-regular fa-heart"></i>
-      </button>
-      <img className="activity__photo" src={attraction.pictures[0]} alt="activity img" />
-      <footer className="activity__details">
-        <div className="activity__details-info">
-          <i className="fa-solid fa-location-dot"></i>
-          <h3>{attraction.city}, {attraction.country}</h3>
-        </div>
-        <div className="activity__details-info">
-          <i className="fa-solid fa-thumbs-up"></i>
-          <h3> {attraction.rating} </h3>
-        </div>
-        {/* <!-- Add enhanced item here!! --> */}
-      </footer>
-    </div >
-
-
+    <li>
+      < div className="activity-list__item" key={attraction.attraction_id} >
+        <button className="fav-button">
+          <i className="fa-regular fa-heart"></i>
+        </button>
+        <img className="activity__photo" src={attraction.pictures[0]} alt="activity img" />
+        <footer>
+          <div className="activity__details">
+            <div className="activity__details-info">
+              <i className="fa-solid fa-location-dot"></i>
+              <h3>{attraction.city}, {attraction.country}</h3>
+            </div>
+            <div className="activity__details-info">
+              <i className="fa-solid fa-thumbs-up"></i>
+              <h3> {attraction.rating} </h3>
+            </div>
+          </div>
+          <EnhancedActivitiesListItem description={attraction.description} price={attraction.price} />
+        </footer>
+      </div >
+    </li>
   );
 
 
