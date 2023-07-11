d7c257132776313b0b41a318920e833493fd9000
Tamara Tell <2telltamara@gmail.com>
2023-07-10 23:24:55 -0400

    fix UserContext, now passing user id accross entire app

--- client/src/hooks/UserContext.js
+++ client/src/hooks/UserContext.js
@@ -3,41 +3,31 @@ import { loginUser } from './loginUser';
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const storedUser = JSON.parse(localStorage.getItem('user'));
-
-  const [userEmail, setUserEmail] = useState(storedUser ? storedUser.email : null);
-  const [userID, setUserID] = useState(storedUser ? storedUser.userId : null);
+  const getUser = () => JSON.parse(localStorage.getItem('user'));
+  const [user, setUser] = useState(getUser());
 
   const handleLogin = async (credentials) => {
     try {
       const data = await loginUser(credentials);
-      setUserEmail(data.email);
-      setUserID(data.userId);
 
       const user = {
         email: data.email,
         id: data.userId
       };
+
       // Persist user data in local storage
       localStorage.setItem('user', JSON.stringify(user));
+      setUser(getUser());
+
     } catch (error) {
       console.error(error);
     }
   };
 
   const logoutHandler = () => {
-    setUserEmail(null);
-    setUserID(null);
-
     localStorage.removeItem('user');
+    setUser(getUser());
   };
-
-  const user = {
-    email: userEmail,
-    id: userID
-  };
-
-  console.log(user);
 
   return (
     <UserContext.Provider value={{ user, handleLogin, logoutHandler }}>
