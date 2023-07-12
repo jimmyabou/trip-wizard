4f49d7a97d742c2f14588c38aa932a013fec9b97
Tamara Tell <2telltamara@gmail.com>
2023-07-12 13:37:36 -0400

    install react modal

--- client/src/components/ActivitiesListFavButton.jsx
+++ client/src/components/ActivitiesListFavButton.jsx
@@ -1,19 +1,26 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
 import { AttractionsContext } from '../providers/AttractionsContext';
+import UserContext from '../providers/UserContext';
 
 const ActivitiesListFavButton = ({ attraction_id }) => {
   const { handleFavAttraction, favAttractionIds } = useContext(AttractionsContext);
+  const { user } = useContext(UserContext);
 
 
-  const toggleFavorite = (attraction_id) => {
-    handleFavAttraction(attraction_id);
+  const toggleFavorite = (attraction_id, user) => {
+    if (user) {
+      handleFavAttraction(attraction_id);
+    } else {
+      alert("Must be logged in to favorite attractions!");
+    }
   };
 
   const isFav = favAttractionIds.includes(attraction_id);
 
 
   return (
-    <button className="fav-button" onClick={() => toggleFavorite(attraction_id)}>
+    <button className="fav-button" onClick={() => toggleFavorite(attraction_id, user)}>
       <i className={`fa ${isFav ? 'fa-solid' : 'fa-regular'} fa-heart`}></i>
     </button>
   );
