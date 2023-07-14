ac057ff57e5bec099259326a01438eac2ef1a4f6
Tamara Tell <2telltamara@gmail.com>
2023-07-14 00:01:52 -0400

    add homepage link to logo

--- client/src/components/NavLogo.jsx
+++ client/src/components/NavLogo.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import logo from '../logo.jpg';
+import { useNavigate } from 'react-router-dom';
 
 
 const NavLogo = () => {
 
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate('/');
+  };
+
   return (
-    <div className="logo">
+    <div className="logo" onClick={handleLogoClick} style={{ cursor: "pointer" }}>
       <img src={logo} alt="Trip-Wizard logo" />
       <h1>TripWizard</h1>
     </div>
