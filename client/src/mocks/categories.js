7d6b8e61ef0a6f370fb3722c36b4ca42395967c6
Tamara Tell <2telltamara@gmail.com>
2023-07-17 13:45:18 -0400

    change order filters

--- client/src/mocks/categories.js
+++ client/src/mocks/categories.js
@@ -1,7 +1,7 @@
 const categories = [
-  { "name": "Sightsee", "icon": "fa-solid fa-binoculars" },
   { "name": "Rating", "icon": "fa-solid fa-star" },
   { "name": "Budget", "icon": "fa-solid fa-piggy-bank" },
+  { "name": "Sightsee", "icon": "fa-solid fa-binoculars" },
   { "name": "Tasting", "icon": "fa-sharp fa-solid fa-wine-glass" },
   { "name": "Luxury", "icon": "fa-solid fa-sack-dollar" },
   { "name": "Cultural", "icon": "fa-solid fa-landmark" },
